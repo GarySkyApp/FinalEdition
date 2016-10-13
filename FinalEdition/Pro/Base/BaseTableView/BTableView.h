9bd9f69ddb4be5833871d8b05cdfc47c73b3ba94
He Hai <hehai@vip.126.com>
2016-10-13 18:56:58 +0800

    协调

--- FinalEdition/Pro/Base/BaseTableView/BTableView.h
+++ FinalEdition/Pro/Base/BaseTableView/BTableView.h
@@ -6,16 +6,35 @@
 //  Copyright © 2016年 J. All rights reserved.
 //
 
+/*
+ 注意，接受的数据结构为：{@“dataList”:...}
+ */
+
 #import <UIKit/UIKit.h>
 #import "BTableViewCell.h"
+#import "PostModel.h"
 
 typedef NSInteger (^sectionNum)();
 typedef NSInteger (^itemsOfSection)(UITableView *tableView, NSInteger section);
 typedef BTableViewCell * (^cellOfIndexPath)(UITableView *tableView, NSIndexPath *indexPath);
+
+//
+typedef void (^headerBlcok)();
+typedef void (^footerBlcok)();
 
 @interface BTableView : UITableView
 
 -(void)configBlock:(sectionNum)section
     itemsOfSection:(itemsOfSection)items
               cell:(cellOfIndexPath)cells;
+-(void)configMjRefreshHeader:(headerBlcok)mjheader andFooter:(footerBlcok)mjfooter;
+
+
+-(void) setNewData:(PostModel *)model;
+-(void) setMoreData:(PostModel *)model;
+
+-(void)noMoreData;
+-(void)resetNoMoreData;
+-(void)stopHeaderRefresh;
+-(void)stopFooterRefresh;
 @end
--- FinalEdition/Pro/Base/BaseTableView/BTableView.m
+++ FinalEdition/Pro/Base/BaseTableView/BTableView.m
@@ -8,6 +8,8 @@
 
 
 #import "BTableView.h"
+#import "MJRefreshAutoNormalFooter.h"
+#import "MJRefreshNormalHeader.h"
 
 @interface BTableView ()
 <
@@ -39,6 +41,17 @@ UITableViewDataSource
     _itemOfSectionBlock = items;
     _cellBlock = cells;
 }
+-(void)configMjRefreshHeader:(headerBlcok)mjheader andFooter:(footerBlcok)mjfooter{
+    MJRefreshNormalHeader *header = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
+        mjheader();
+    }];
+    MJRefreshAutoNormalFooter *footer = [MJRefreshAutoNormalFooter footerWithRefreshingBlock:^{
+        mjfooter();
+    }];
+    self.mj_header = header;
+    self.mj_footer = footer;
+}
+
 
 #pragma mark - tableView DataSource
 -(NSInteger) numberOfSections{
@@ -52,4 +65,17 @@ UITableViewDataSource
     return cell;
 }
 
+#pragma mark - interface
+-(void)noMoreData{
+    [self.mj_footer endRefreshingWithNoMoreData];
+}
+-(void)resetNoMoreData{
+    [self.mj_footer resetNoMoreData];
+}
+-(void)stopHeaderRefresh{
+    [self.mj_header endRefreshing];
+}
+-(void)stopFooterRefresh{
+    [self.mj_footer endRefreshing];
+}
 @end
